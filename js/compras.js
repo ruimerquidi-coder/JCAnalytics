b3a63752242e136f17c45321da7649d9935d1403
ruimerquidi-coder <ruimerquidi@gmail.com>
2026-07-03 13:09:59 -0300

    Update compras.js

--- js/compras.js
+++ js/compras.js
@@ -1,1 +1,75 @@
+function gerarListaCompras(produtos) {
 
+    const lista = [];
+
+    produtos.forEach(produto => {
+
+        const estoque = Number(produto["Estoque"] || 0);
+        const minimo = Number(produto["Estoque Mín"] || 0);
+
+        if (estoque < minimo) {
+
+            lista.push({
+
+                nome: produto["Nome"],
+
+                categoria: produto["Categoria"],
+
+                estoque: estoque,
+
+                minimo: minimo,
+
+                comprar: minimo - estoque
+
+            });
+
+        }
+
+    });
+
+    lista.sort((a, b) => b.comprar - a.comprar);
+
+    return lista;
+
+}
+
+function mostrarListaCompras(lista) {
+
+    const div = document.getElementById("listaCompras");
+
+    if (lista.length === 0) {
+
+        div.innerHTML = "<p>Nenhum produto precisa ser comprado.</p>";
+
+        return;
+
+    }
+
+    let html = "";
+
+    lista.forEach(item => {
+
+        html += `
+            <div class="border-bottom py-2">
+
+                <strong>${item.nome}</strong><br>
+
+                Categoria: ${item.categoria}<br>
+
+                Estoque: ${item.estoque} |
+                Mínimo: ${item.minimo} |
+
+                <span style="color:red;font-weight:bold;">
+
+                    Comprar ${item.comprar} unidades
+
+                </span>
+
+            </div>
+        `;
+
+    });
+
+    div.innerHTML = html;
+
+}
